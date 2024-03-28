74ae3e4e15f4395ec5dcee9dc26de9de11c2bd74
Tiago Amaral <tiago.amaral@kmee.com.br>
2024-03-28 13:42:54 -0300

    [ADD] admins

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Produto, Cliente, Entregador, Pedido
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'descricao', 'quantidade')
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'endereco', 'whatsapp')
+
+class EntregadorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'telefone', 'cpf')
+
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ('cliente', 'entregador', 'status')
+
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Entregador, EntregadorAdmin)
+admin.site.register(Pedido, PedidoAdmin)
